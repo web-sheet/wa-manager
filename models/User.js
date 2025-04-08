39baca3f34af2e6836a06c1db5f2298916f25451
web-sheet <websheetweb@gmail.com>
2025-04-08 14:36:58 +0700

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,12 @@
+// models/user.js
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  number: { type: String, required: true, unique: true },
+  time: { type: String },
+  status: { type: String, default: 'offline' },
+  webhookUrl: { type: String },
+  userType: { type: String, required: true }   
+});
+
+export const User = mongoose.model('User', userSchema);
