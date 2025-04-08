458b9ca08a2ac30f4dc9f74ea3b0f95d3fa395b1
web-sheet <websheetweb@gmail.com>
2025-04-08 14:38:38 +0700

    Update main.js

--- main.js
+++ main.js
@@ -387,7 +387,7 @@ app.get('/checkSession', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
